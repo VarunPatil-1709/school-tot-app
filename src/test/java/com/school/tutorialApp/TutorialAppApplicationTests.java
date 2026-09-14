e8d06c95a480248a3c658eeb3ca6449bd3ffc5eb
mechack2022 <dtoms624@gmail.com>
2026-09-14 15:20:24 +0100

    app setup and packages created

---
+++ src/test/java/com/school/tutorialApp/TutorialAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.tutorialApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
